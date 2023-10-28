ab9f853edb15a0669dda918bec68d030f8768665
kalikrit <kalikrit@gmail.com>
2023-10-29 01:01:41 +0300

    structure, vite.config, pages, hundlebars,scss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [handlebars()],
-})
+  plugins: [handlebars({
+    partialDirectory: resolve(__dirname, 'src/partials'),
+    context: {global: 'global'}
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login/login.html'),
+        registration: resolve(__dirname, 'src/pages/registration/registration.html'),
+        error404: resolve(__dirname, '/src/pages/errors/404.html'),
+        error500: resolve(__dirname, '/src/pages/errors/500.html'),
+      }
+    }
+  },
+  server: {
+    open: '/src/pages/login/signup.html'
+  }
+}) 

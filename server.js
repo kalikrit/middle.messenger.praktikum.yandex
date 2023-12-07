130e319783e5d85433acf2d3002bac7ad62a972e
kalikrit <kalikrit@gmail.com>
2023-12-07 22:06:37 +0300

    fix linter

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ app.use(express.static('./dist'));
 app.use('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+app.listen(PORT, () => {
+  // console.log(`Example app listening on port ${PORT}!`);
+});
